a836052b1cbd5eb951af126843fb225d164eb960
Gustas Brazaitis <gustasbr@gmail.com>
2020-12-30 14:21:03 +0200

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Gustas on 2020-12-30.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
